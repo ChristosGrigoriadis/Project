9c5d80df9fb71a49a86fae642c7895ee1d31dc26
PavlosAntonakos <57539546+paulnakos@users.noreply.github.com>
2020-12-11 15:34:45 +0200

    Edit Venue and MyVenues Pages added

--- UberTappDeveloping/Controllers/VenueController.cs
+++ UberTappDeveloping/Controllers/VenueController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using UberTappDeveloping.DAL;
 using UberTappDeveloping.Models;
 using UberTappDeveloping.ViewModels;
+using System.Data.Entity;
 
 namespace UberTappDeveloping.Controllers
 {
@@ -18,28 +20,98 @@ namespace UberTappDeveloping.Controllers
             context = new ApplicationDbContext();
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
         // GET: Venue
         public ActionResult Index()
         {
             return View();
         }
 
+        //Get: Edit/id
+        public ActionResult Edit(int id)
+        {
+            var venue = context.Venues.SingleOrDefault(v => v.Id == id);
+
+            var viewModel = new VenueFormViewModel
+            {
+                Locations = GetLocations(),
+                ManagerName = GetUser().FirstName + " " + GetUser().LastName,
+                Venue = venue
+            };
+
+            return View("VenueForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Venue venue)
+        {
+            if (venue == null)
+                return HttpNotFound();
+
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new VenueFormViewModel
+                {
+                    Locations = GetLocations(),
+                    ManagerName = GetUser().FirstName + " " + GetUser().LastName,
+                    Venue = venue
+                };
+                return View("VenueForm", viewModel);
+            }
+
+            var venueDb = context.Venues.SingleOrDefault(v => v.Id == venue.Id);
+
+            venueDb.Name = venue.Name;
+            venueDb.DateOpened = venue.DateOpened;
+            venueDb.Manager = venue.Manager;
+            venueDb.LocationId = venue.LocationId;
+
+
+
+            context.SaveChanges();
+
+
+            return RedirectToAction("Index", "home");
+        }
+
+        public ActionResult UserVenues()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var userVenues = context.Venues
+                .Include(v => v.Location)
+                .Where(v => v.OwnerId == userId);
+
+            return View(userVenues);
+        }
+
         private IEnumerable<object> GetLocations()
         {
             return context.Locations
                 .Select(l => new { value = l.Id, text = l.Country + " | " + l.City + " | " + l.AddressLine1 + " " + l.AddressNumber });
+            //    as IEnumerable<object>;
+        }
+
+        private ApplicationUser GetUser()
+        {
+            var userId = User.Identity.GetUserId();
+            return context.Users.SingleOrDefault(u => u.Id == userId);
         }
 
         //GET : venue/new
+        [Authorize(Roles = "Venue Owner")]
         public ActionResult New()
         {
-            //var list = context.Locations
-            //    .Select(l => new { value = l.Id, text = l.Country + " | " + l.City + " | " + l.AddressLine1 + " " + l.AddressNumber })
-            //    as IEnumerable<object>;
-
+            
             var viewModel = new VenueFormViewModel
             {
-                Locations = GetLocations()
+                Locations = GetLocations(),
+                ManagerName = GetUser().FirstName + " " + GetUser().LastName,
+                Venue = new Venue()
             };
 
 
@@ -47,17 +119,24 @@ namespace UberTappDeveloping.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "Venue Owner")]
         public ActionResult New(Venue venue)
         {
             if (!ModelState.IsValid)
             {
-                var viewModel = new VenueFormViewModel { Locations = GetLocations() };
+                var viewModel = new VenueFormViewModel
+                {
+                    Locations = GetLocations(),
+                    ManagerName = GetUser().FirstName + " " + GetUser().LastName,
+                    Venue = new Venue()
+                };
                 return View("VenueForm", viewModel);
             }
 
             if (venue == null)
                 return HttpNotFound();
 
+            venue.OwnerId = User.Identity.GetUserId();
             context.Venues.Add(venue);
             context.SaveChanges();
 
--- UberTappDeveloping/ViewModels/VenueFormViewModel.cs
+++ UberTappDeveloping/ViewModels/VenueFormViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System.Web.Mvc;
+using UberTappDeveloping.Controllers;
 using UberTappDeveloping.Models;
 
 namespace UberTappDeveloping.ViewModels
@@ -12,6 +15,20 @@ namespace UberTappDeveloping.ViewModels
 
         public IEnumerable<object> Locations { get; set; }
 
+        public string ManagerName { get; set; }
+        
+        public string ActionName
+        {
+            get
+            {
+                Expression<Func<VenueController, ActionResult>> Update = vc => vc.Update(new Venue());
+                Expression<Func<VenueController,ActionResult>> New = vc => vc.New();
 
+                var action = Venue.Id == 0 ? New : Update;
+                var actionName = (action.Body as MethodCallExpression).Method.Name;
+
+                return actionName;
+            }
+        }
     }
 }
