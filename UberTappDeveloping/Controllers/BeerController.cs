8cd15c9bfe83dc9a1ef5fe6391d3b76b492ab5ee
AkisRep <62769864+AkisRep@users.noreply.github.com>
2020-12-30 20:55:29 +0200

    Beer CRUDs

--- UberTappDeveloping/Controllers/BeerController.cs
+++ UberTappDeveloping/Controllers/BeerController.cs
@@ -3,15 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UberTappDeveloping.DAL;
+using UberTappDeveloping.Models;
+using UberTappDeveloping.ViewModels;
 
 namespace UberTappDeveloping.Controllers
 {
     public class BeerController : Controller
     {
-        // GET: Beer
-        public ActionResult Index()
+        private ApplicationDbContext context;
+
+        public BeerController()
         {
-            return View();
+            context = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+
+
+        #region GET
+        // GET: Beer
+        public ActionResult Beers()
+        {
+            var beers = context.Beers.ToList();
+
+            return View(beers);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var viewModel = new BeerViewModel(context.Beers.SingleOrDefault(b => b.Id == id));
+
+            if (viewModel == null)
+                return HttpNotFound();
+
+            return View("BeerForm", viewModel);
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new BeerViewModel();
+
+            return View("BeerForm", viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var beer = context.Beers.SingleOrDefault(b => b.Id == id);
+            return View(beer);
+        }
+
+        #endregion
+
+
+        #region POST
+
+        public ActionResult Save(Beer beer)
+        {
+            if (beer.Id == 0)
+                context.Beers.Add(beer);
+            else
+            {
+                var beerToBeUpdated = context.Beers.SingleOrDefault(b => b.Id == beer.Id);
+                beerToBeUpdated.Name = beer.Name;
+                beerToBeUpdated.ABV = beer.ABV;
+                beerToBeUpdated.IBU = beer.IBU;
+                beerToBeUpdated.EBC = beer.EBC;
+                beerToBeUpdated.Description = beer.Description;
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Beers","Beer");
+        }
+
+
+        #endregion
+
+
     }
 }
---
+++ UberTappDeveloping/ViewModels/BeerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UberTappDeveloping.Models;
+
+namespace UberTappDeveloping.ViewModels
+{
+    public class BeerViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public int? EBC { get; set; } // European Brewery Convention -Colour-
+        public double? ABV { get; set; } // Alcohol By Volume 3% - 13% for beers
+        public int? IBU { get; set; } // International Bitterness Units
+        public string Description { get; set; }
+
+        public BeerViewModel(Beer beer)
+        {
+            Id = beer.Id;
+            Name = beer.Name;
+            EBC = beer.EBC;
+            ABV = beer.ABV;
+            Description = beer.Description;
+        }
+
+        public BeerViewModel() { }
+    }
+}
