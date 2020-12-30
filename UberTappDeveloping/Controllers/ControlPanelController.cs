404d2069ee40aee67ac04f2799d88971a8113d73
AkisRep <62769864+AkisRep@users.noreply.github.com>
2020-12-30 16:29:39 +0200

    Merged Kots & Beer Controller

---
+++ UberTappDeveloping/Controllers/ControlPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UberTappDeveloping.Controllers
+{
+    public class ControlPanelController : Controller
+    {
+        // GET: ControlCenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UberTappDeveloping/ViewModels/ControlPanelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UberTappDeveloping.Models;
+
+namespace UberTappDeveloping.ViewModels
+{
+    public class ControlPanelViewModel
+    {
+        public ApplicationUser ActiveUser { get; set; }
+        public ICollection<ApplicationUser> AllUsers { get; set; }
+        public ICollection<Beer> AllBeers { get; set; }
+        public ICollection<Venue> AllVenues { get; set; }
+        public ICollection<UserBeer> MyFavBeers { get; set; }
+        
+    }
+}
